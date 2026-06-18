e08dd7d11734f2bc02d23d7b39d95e53b269c004
Greick Padua <greick.padua@escola.pr.gov.br>
2026-06-18 10:03:35 -0300

    Add files via upload

---
+++ indexof.js
@@ -0,0 +1,44 @@
+// Utilize o método indexOf() para encontrar a posição do número 25 no vetor abaixo.
+// let numeros = [10, 15, 20, 25, 30];
+
+
+// Utilize o método indexOf() para verificar em qual índice a palavra "banana" aparece no vetor abaixo.
+// let frutas = ["maçã", "uva", "banana", "laranja"];
+
+
+// Utilize o método indexOf() para descobrir se o número 100 existe no vetor abaixo
+// let numeros = [5, 10, 15, 20, 25];
+
+
+// Utilize o método indexOf() para encontrar a posição da letra "c" no vetor abaixo.
+// let letras = ["a", "b", "c", "d", "e"];
+
+
+// Utilize o método indexOf() para descobrir o índice da primeira ocorrência do número 7 no vetor abaixo
+// let numeros = [3, 7, 9, 7, 12, 7];
+
+//ex1
+let numerosum = [10, 15, 20, 25, 30];
+let posicaoum =numerosum.indexOf(25)
+console.log(posicaoum)
+
+
+//ex2
+let frutasum = ["maçã", "uva", "banana", "laranja"];
+let posicaodois=frutasum.indexOf("banana")
+console.log(posicaodois)
+
+//ex3
+let numerosdois = [5, 10, 15, 20, 25];
+let posicaotres=numerosdois.indexOf(100)
+console.log(posicaotres)
+
+//ex4
+let letras = ["a", "b", "c", "d", "e"];
+let posicaoquatro=letras.indexOf("c")
+console.log(posicaoquatro)
+
+//ex5
+let numerostres = [3, 7, 9, 7, 12, 7];
+let posicaocinco=numerostres.indexOf(7)
+console.log(posicaocinco)
